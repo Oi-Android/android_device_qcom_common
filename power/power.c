27ee91844e5bbc81fdbd3f1ac6d7c1d62d47219c
martincz <martincz@mokeedev.com>
2014-11-25 03:13:18 +0800

    mokee bringup
    Change-Id: If85458217e052f971a4ac0f4b5c3624abe956a73

--- power/power.c
+++ power/power.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
- * Copyright (c) 2014, The CyanogenMod Project
+ * Copyright (c) 2014, The MoKee OpenSource Project
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
